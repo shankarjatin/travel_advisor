4b786c4bbbafa38eab7afd2dd9d3c176dbea62c1
arpittyagi-19 <arpittyagi9306@gmail.com>
2024-04-11 11:41:24 +0530

    basic structure and Header

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import {paper, Typography, useMediaQuery} from '@material-ui/core'
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined'
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles'
+
+const Map = ()=>{
+    return (
+        <h1>
+         Map
+        </h1>
+    )
+}
+
+export default Map;
